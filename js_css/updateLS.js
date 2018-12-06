b01d756b02e4f1ea770856fc3c93f18bc3de04c7
gwagdanny <32524655+gwagdanny@users.noreply.github.com>
2018-12-05 17:40:58 -0800

    tag added

--- js_css/updateLS.js
+++ js_css/updateLS.js
@@ -1,3 +1,4 @@
+
 if (localStorage.getItem("Uname")) {
   document.getElementById("getName").innerHTML = localStorage.getItem("Uname");
 } else {
@@ -8,26 +9,4 @@ if (localStorage.getItem("Uemail")) {
   document.getElementById("getEmail").innerHTML = localStorage.getItem("Uemail");
 } else {
   document.getElementById("getEmail").innerHTML = "-";
-}
-
-if (localStorage.getItem("Utags")) {
-  // Retrieving UTags
-  var userTags = [];
-  userTags = JSON.parse(localStorage.getItem("Utags"));
-  // alert(userTags);
-
-  var source = document.getElementById("tagButton").innerHTML;
-  var template = Handlebars.compile(source);
-
-  var parentDiv = $("#templatedButton");
-
-  for (var i = 0; i < userTags.length; i++) {
-    var curData = userTags[i];
-    // alert("here");
-    var curHtml = template(curData);
-    parentDiv.append(curHtml);
-  }
-  // document.getElementById("getTags").innerHTML = userTags;
-} else {
-  document.getElementById("getTags").innerHTML = "-";
 }
